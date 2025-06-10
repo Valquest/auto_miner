2edc96c5a44e0c88a387485394745bdbdceb78d8
Dovydas Valančius <dovydas.valancius@gmail.com>
2025-06-10 08:00:05 +0300

    - Refactoring: Refactored main.py

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
-from automations import ore_finder, inv_mover, idling
+"""
+Orchestrates all major activities for sucesfull mining
+"""
+
 import time
-import random
-from pyautogui import ImageNotFoundException
+
+from automations import ore_finder, inv_mover, idling
 from datetime import datetime
+from pyautogui import ImageNotFoundException
+
 
 if __name__ == "__main__":
 
     # Create a idle tracker
     idler = idling.Idler()
 
+    # Keep looping untill indicated to stop
     while True:
         try:
             time.sleep(4)
             idler.rest()
+
+            # Handles mining operations
             ore_finder.mine(retriever=True)
+
+            # Handles inventory transfer form ship to refinery operations
             inv_mover.items_to_refinery()
+
+        # Handles exceptions
         except ImageNotFoundException as e:
             time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             print(f"Faced an exception on {time}. \nError:{e}")
