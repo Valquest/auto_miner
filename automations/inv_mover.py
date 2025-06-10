2db44eafe9ddf091568fb73a80d317ff3d1674f1
Dovydas Valančius <dovydas.valancius@gmail.com>
2025-06-10 08:04:01 +0300

    - Documenting: Code documented

--- automations/inv_mover.py
+++ automations/inv_mover.py
@@ -1,3 +1,7 @@
+"""
+Handles actions needed to move inventory from ship to refinery
+"""
+
 import pyautogui
 import random
 import time
@@ -9,9 +13,25 @@ from pyautogui import ImageNotFoundException
 # Create an image loader instance to access image paths
 imgs = Image_loader()
 
+def drag_items_to_refinery_inv()->None:
+    """
+    Item icon draging from one inv. to another
+    return:
+        None
+    """
+    # Add randomness in where mouse click each time to avoid robot like behavior
+    random_num = round(random.uniform(0.5, 1.5))
+    try:
+        x, y = pyautogui.locateCenterOnScreen(imgs.item_drop_destination, confidence=0.85)
+        pyautogui.dragTo(x, y - 200 + random_num, duration=random_num, button='left')
+    except ImageNotFoundException:
+        print("Failed to drag items from ships inv. to refineries inv. Failed to locate image: {imgs.item_drop_destination}")
+
 def items_to_refinery()->None:
     """
     Handles action chain for moving ships inventory to refineries inventory
+    return:
+        None
     """
     # TODO: Remove pre-sleep from action chain
     time.sleep(2)
@@ -47,14 +67,8 @@ def items_to_refinery()->None:
 def stack_items()->None:
     """
     Stacks items in ships inventory
+    return:
+        None
     """
     ore_finder.mouse_action(imgs.mining_hold_btn, "rightClick", offset_x=175, rand_moves=0)
     ore_finder.mouse_action(imgs.stack_all_btn, "click", rand_moves=1)
-
-def drag_items_to_refinery_inv()->None:
-    random_num = round(random.uniform(0.5, 1.5))
-    try:
-        x, y = pyautogui.locateCenterOnScreen(imgs.item_drop_destination, confidence=0.85)
-        pyautogui.dragTo(x, y - 200 + random_num, duration=random_num, button='left')
-    except ImageNotFoundException:
-        print("Failed to drag items from ships inv. to refineries inv. Failed to locate image: {imgs.item_drop_destination}")
